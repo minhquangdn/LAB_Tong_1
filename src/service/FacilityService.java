e0b19e14e41077ecde9bff1980dec2eb7a19663b
minhquangdn <minhquangdn27@gmail.com>
2024-02-25 17:11:13 +0700

    Update FacilityService.java

--- src/service/FacilityService.java
+++ src/service/FacilityService.java
@@ -3,12 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.Facility;
+import model.House;
+import model.Room;
+import model.Villa;
 
 /**
  *
  * @author DELL
  */
-public interface FacilityService {
+public class FacilityService {
+
+    static List<Facility> listFacility = new ArrayList<>();
+
+
     public void displayFacility();
     public void addNewFacility();
     public void editFacility();
