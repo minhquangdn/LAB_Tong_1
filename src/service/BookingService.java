58a2814bb7a8a74f9d3213a3a870a6da4b0a4e2e
Thaihung204 <136320284+Thaihung204@users.noreply.github.com>
2024-02-25 15:51:07 +0700

    Update BookingService.java

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -15,22 +15,26 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeSet;
 import model.Booking;
+import model.Constract;
 
 public class BookingService {
 
     TreeSet<Booking> listBooking = new TreeSet<>();
     Queue<Booking> bookingBill = new LinkedList<>();
-    ArrayList<String> listConstract = new ArrayList<>();
-    Map<Facility, Integer> bill;
+    ArrayList<Constract> listConstract = new ArrayList<>();
+    Map<Facility, Integer> bill = new HashMap<>();
     Map<Booking, Integer> oorder = new HashMap<>();  //luu cac phong tromg 1 don
     Scanner in = new Scanner(System.in);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    FacilityService fc = new FacilityService();
 
     public BookingService() {
     }
 
     public void readInf() throws ParseException {
-        Room r = new Room("Room", "RO001", "room", 0, 0, 0, "Luxury");
+        Facility r = fc.searchFacility("RO001");
+        System.out.println(r);
         bill.put(r, 2);
         listBooking.add(new Booking("0001", checkdate("20/2/2024"), checkdate("21/2/2024"), checkdate("25/2/2024"), "KH0001", bill));
         listBooking.add(new Booking("0002", checkdate("19/2/2024"), checkdate("23/2/2024"), checkdate("27/2/2024"), "KH0002", bill));
@@ -43,67 +47,108 @@ public class BookingService {
         String bookingId, customerId, serviceId;
         Date checkinDate, checkoutDate, now;
         Facility f;
-        bill.clear();
+
         // ID
         do {
             bookingId = createBooingId();
         } while (!checkExistBookingId(bookingId));
 
         //Curent date
+        now = new Date();
+        System.out.println(now);
+        //check in-out
+        do {
+            System.out.println("Input Checkin Date: ");
+            checkinDate = getDateFromInput();
+            if (now.after(checkinDate)) {
+                System.out.println("Please input checkin date after today!");
+            }
+        } while (checkinDate.before(now));
+
+        do {
+            System.out.println("Input Checkout Date: ");
+            checkoutDate = getDateFromInput();
+            if (checkoutDate.before(checkinDate)) {
+                System.out.println("Please input checkout date after checkin");
+            }
+        } while (checkoutDate.before(checkinDate));
+        //------display customer id ......
+        System.out.println("Input customer Id: ");
+        customerId = in.nextLine();
         do {
 
-            now = new Date();
-            System.out.println(now);
-            //check in-out
-            do {
-                System.out.println("Input Checkin Date: ");
-                checkinDate = getDateFromInput();
-                if (now.after(checkinDate)) {
-                    System.out.println("Please input checkin date after today!");
-                }
-            } while (checkinDate.before(now));
-
-            do {
-                System.out.println("Input Checkout Date: ");
-                checkoutDate = getDateFromInput();
-                if (checkoutDate.before(checkinDate)) {
-                    System.out.println("Please input checkout date after checkin");
-                }
-            } while (checkoutDate.before(checkinDate));
-            //------display customer id ......
-            System.out.println("Input customer Id: ");
-            customerId = in.nextLine();
             //------display facility id......
+            fc.displayFacility();//code quang
             System.out.println("Input service Id: ");
 
             serviceId = in.nextLine();
-            f = FacilityService.search(serviceId); // return room or vila or house
-            
+            f = fc.searchFacility(serviceId); // return room or vila or house
+            System.out.println(f);
             System.out.println("Input quantity: "); // check quan ddur k
             int quan = in.nextInt();
-            
             bill.put(f, quan);
 
-        } while (!confirm("continute booking"));
+            in.nextLine();
+
+        } while (confirm("continute booking"));
         Booking b = new Booking(bookingId, now, checkinDate, checkoutDate, customerId, bill);
         listBooking.add(b);
+        //System.out.println(b);
         System.out.println("Booking complete successful!");
         System.out.println("Your Booking ID is: " + bookingId);
+        //bill.clear();
         //------display room inf
     }
-//------------------------------------------------------------------------------   
+//------------------------------------------------------------------------------ 
+
+    public int caculateTotal(String bookingId) {
+        int total = 0;
+        Booking b = searchBooking(bookingId);
+        Map<Facility, Integer> bill = b.getBill();
+        for (Map.Entry<Facility, Integer> entry : bill.entrySet()) {
+            Facility f = entry.getKey();
+            Integer value = entry.getValue();
+
+            total += f.getPrice() * value;
+        }
+        return total;
+    }
 
 //------------------------------------------------------------------------------   
     public void displayListBooking() {
         System.out.println("Booking List: ");
-        System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
-        System.out.printf("| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n", "Booking ID", "Booking Date", "Check-in Date", "Check-out Date", "Customer ID", "Service ID");
-        System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
-//         System.out.println(listBooking);  
+        System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+        System.out.printf("| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n", "Booking ID", "Booking Date", "Check-in Date", "Check-out Date", "Customer ID", "Service ID", "Quantity");
+
         for (Booking b : listBooking) {
-            System.out.printf("| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n", b.getBookingId(), dateFormat.format(b.getBookingDate()), dateFormat.format(b.getStart()), dateFormat.format(b.getFinish()), b.getCusId(), b.getBill().get(b));
-            System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+            Map<Facility, Integer> bill = b.getBill();
+            for (Map.Entry<Facility, Integer> entry : bill.entrySet()) {
+                Facility f = entry.getKey();
+                Integer value = entry.getValue();
+                System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+                System.out.printf("| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n", b.getBookingId(), dateFormat.format(b.getBookingDate()), dateFormat.format(b.getStart()), dateFormat.format(b.getFinish()), b.getCusId(), f.getName(), value);
+                
+            }
+            System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+
+            System.out.println("|TOTAL: " + caculateTotal(b.getBookingId()) + "\n");
         }
+
+    }
+//------------------------------------------------------------------------------ 
+
+    public void displayBooking(String bookingId) {
+        Booking b = searchBooking(bookingId);
+        Map<Facility, Integer> bill = b.getBill();
+        for (Map.Entry<Facility, Integer> entry : bill.entrySet()) {
+            Facility f = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+            System.out.printf("| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n", b.getBookingId(), dateFormat.format(b.getBookingDate()), dateFormat.format(b.getStart()), dateFormat.format(b.getFinish()), b.getCusId(), f.getName(), value);
+            
+        }
+        System.out.println("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+");
+
 
     }
 //------------------------------------------------------------------------------ 
@@ -114,9 +159,18 @@ public class BookingService {
             System.out.println("Input Booking Id :");
             bookingId = in.nextLine();
         } while (!checkExistBookingId(bookingId));
-
-        System.out.println("Your deposit amount is: ");
-
+        constractId=createConstractId();
+        displayBooking(bookingId);
+        System.out.println("Your Deposit amount is 50% : " + caculateTotal(bookingId) * 0.5);
+        System.out.println("Your Total amount is : " + caculateTotal(bookingId));
+        Constract c = new Constract(constractId, bookingId, caculateTotal(bookingId) * 50 / 100, caculateTotal(bookingId));
+        if(confirm("create constract")){
+            listConstract.add(c); 
+            System.out.println("Constract have been created !");
+        }
+       
+        else
+            System.out.println("Constract have not been created !");
     }
 
 //------------------------------------------------------------------------------ 
@@ -129,15 +183,35 @@ public class BookingService {
         }
         return String.valueOf(max + 1);
     }
+//------------------------------------------------------------------------------ 
+    public String createConstractId() {
+        int max = 0;
+        for (Constract c : listConstract) {
+            if (Integer.parseInt(c.getConstractId()) > max) {
+                max = Integer.parseInt(c.getConstractId());
+            }
+        }
+        return String.valueOf(max + 1);
+    }
 //------------------------------------------------------------------------------  
 
     public Boolean checkExistBookingId(String id) {
         for (Booking b : listBooking) {
-            if (b.getBookingId().equals(id)) {
+            if (b.getBookingId().equals(b)) {
                 return false;
             }
         }
         return true;
+    }
+//------------------------------------------------------------------------------  
+
+    public Booking searchBooking(String bookingId) {
+        for (Booking b : listBooking) {
+            if (b.getBookingId().equalsIgnoreCase(bookingId)) {
+                return b;
+            }
+        }
+        return null;
     }
 //------------------------------------------------------------------------------    
 // check in here
@@ -178,9 +252,11 @@ public class BookingService {
         System.out.println("Do you want to " + msg + " Y/N ?");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+        input.toUpperCase();
         if (input.equalsIgnoreCase("Y")) {
             return true;
         }
         return false;
     }
 }
+
