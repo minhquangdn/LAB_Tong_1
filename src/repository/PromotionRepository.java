e4fa4efc721975f6baac1359d29f6c94cac66186
tranvietthang2994 <tranvietthangmtp@gmail.com>
2024-02-26 23:29:45 +0700

    Thang update lan 10

--- src/repository/PromotionRepository.java
+++ src/repository/PromotionRepository.java
@@ -2,9 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package service;
+package repository;
 
 import model.Facility;
+import service.BookingService;
 import model.Booking;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
