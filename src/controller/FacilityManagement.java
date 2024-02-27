34fb72bfafeaf489d4726b16a2ff3950f616e89f
minhquangdn <150581814+minhquangdn@users.noreply.github.com>
2024-02-27 12:54:41 +0700

    Update FacilityManagement.java

--- src/controller/FacilityManagement.java
+++ src/controller/FacilityManagement.java
@@ -1,13 +1,13 @@
 package controller;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import service.FacilityService;
 import view.Menu;
 
-/**
- *
- * @author DELL
- */
-public class FacilityManagement extends Menu {
+public class FacilityManagement extends Menu{
 
     FacilityService facilityService = new FacilityService();
     FuramaController parentMenu; // Thêm thuộc tính để lưu trữ menu cha
@@ -24,14 +24,22 @@ public class FacilityManagement extends Menu {
     }
 
     @Override
-    public void execute(int n) throws Exception {
+    public void execute(int n) {
         switch (n){
-            case 1 ->{ //"Display list facility"
+            case 1 ->{ try {
+                //"Display list facility"
                 facilityService.displayFacility();
+            } catch (IOException ex) {
+                Logger.getLogger(FacilityManagement.class.getName()).log(Level.SEVERE, null, ex);
+            }
             }
             
-            case 2 ->{ //"Add new facility"
+            case 2 ->{ try {
+                //"Add new facility"
                 facilityService.addNewFacility();
+            } catch (Exception ex) {
+                Logger.getLogger(FacilityManagement.class.getName()).log(Level.SEVERE, null, ex);
+            }
             }
             
             case 3 ->{ //"Display list facility maintenance"
